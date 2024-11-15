5578a7dd129d55ef6fcf8a59ffe3709c32f0b34b
vlfxhd69@naver.com <vlfxhd69@nvaer.com>
2024-11-15 21:07:30 +0900

    feat: 수정사항 업로드

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useKakao } from 'vue3-kakao-maps';
 
 import App from './App.vue'
 import router from './router'
@@ -11,4 +12,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+useKakao('a55f2a7d9703cdee565c4c05eca9def0', ['clusterer', 'services', 'drawing']); // 각 라이브러리는 ',(콤마)'로 구분합니다.
 app.mount('#app')
