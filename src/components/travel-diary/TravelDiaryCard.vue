5d934cb5b49540231cb02c98495029236e9c4919
seonghyunKim <aruesin2@gmail.com>
2024-11-26 07:57:49 +0900

    feature: travel-diary list 조회 기능 추가

--- src/components/travel-diary/TravelDiaryCard.vue
+++ src/components/travel-diary/TravelDiaryCard.vue
@@ -6,8 +6,7 @@
       <p class="text-gray-600 text-sm truncate mb-2">{{ diary.description }}</p>
       <div class="flex items-center justify-between text-gray-400 text-xs">
         <span>{{ diary.username }}</span>
-        <span>{{ diary.createdAt }}</span>
-
+        <span>{{ new Date(diary.createdAt).toLocaleDateString('ko-KR', { year: 'numeric', month: '2-digit', day: '2-digit' }) }}</span>
       </div>
     </div>
   </div>
